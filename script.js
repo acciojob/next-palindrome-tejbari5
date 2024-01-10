75c0f55de95d6f1a755305cfe5a3a04d61ae23f9
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-10 19:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function nextPalindrome(num) {
   //your JS code here.
+	let n=parseInt(num)
+	++n
+	while(!isPalindrome(n)){
+		++n
+	}
+
+	return n;
+
+}
+
+function isPalindrome(num){
+	return num.toString() === num.toString().split('').reverse().join('');
 }
 
 const input = prompt("Enter a palindrome number");
